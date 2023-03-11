2d79d43b2fd05c0c71534da9210ca7ad536cc890
cyd <ceydatoker2209@gmail.com>
2023-03-11 14:38:28 +0200

    user form table added

---
+++ src/api/user-form/controllers/user-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-form controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-form.user-form');
